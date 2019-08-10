587fa58b35fcde8fa5665878991dffe6afba72bc
Piyush Kakkar <kakkarpiyush96@gmail.com>
2019-08-11 03:11:10 +0530

    add content for experience and projects

--- src/components/SubSection.vue
+++ src/components/SubSection.vue
@@ -4,6 +4,10 @@
                :headerData="data.header"
                :title="title"
           />
+          <ul class="content" v-if="Array.isArray(data.content)">
+               <li v-for="(item, i) in data.content" :key="i" v-html="item" />
+          </ul>
+          <p class="content" v-else v-html="data.content" />
      </div>
 </template>
 
@@ -24,11 +28,26 @@ export default {
           },
           title: String,
      },
-     created() {
-          console.log('**', this.data)
-     }
 }
 </script>
 
 <style scoped>
+.sub-section {
+     margin: var(--subsection-space);
+}
+.content {
+     margin: 0;
+     color: var(--light-gray);
+     font-size: 10pt;
+}
+ul.content {
+     padding-left: .4cm;
+     margin-left: 3px;
+}
+ul.content li {
+     margin-bottom: .1cm;
+}
+.content >>> b {
+     font-weight: 800;
+}
 </style>
